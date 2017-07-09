1163e57892b15457d0517f69f4fce3ad42569de4
edugfilho <edugomfilho@gmail.com>
2017-07-09 13:44:46 -0400

    Refactor and toggle breakpoint

--- play-template-breakpoint-plugin/src/PlaceBreakpointOnLine.java
+++ play-template-breakpoint-plugin/src/PlaceBreakpointOnLine.java
@@ -1,27 +1,21 @@
 import com.intellij.debugger.DebuggerManagerEx;
 import com.intellij.debugger.ui.breakpoints.Breakpoint;
 import com.intellij.debugger.ui.breakpoints.BreakpointManager;
-import com.intellij.debugger.ui.breakpoints.LineBreakpoint;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.editor.Document;
 import com.intellij.openapi.editor.Editor;
-import com.intellij.openapi.editor.impl.EditorImpl;
 import com.intellij.openapi.fileEditor.FileDocumentManager;
 import com.intellij.openapi.fileEditor.FileEditorManager;
 import com.intellij.openapi.vfs.VirtualFile;
 
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.Map;
-import java.util.Set;
 
-/**
- * Created by Eduardo on 2017-07-08.
- */
 public class PlaceBreakpointOnLine extends AnAction {
 
-    private static Map<String, Breakpoint> activeBreakpoints = new HashMap<>();
+    private static Map<String, Breakpoint> activeBreakpointsOnSource = new HashMap<>();
+    private static Map<String, Breakpoint> activeBreakpointsOnTarget = new HashMap<>();
 
 
     private String makeKey(String fileName, int lineNumber) {
@@ -33,33 +27,52 @@ public class PlaceBreakpointOnLine extends AnAction {
         Editor currentEditor = FileEditorManager.getInstance(e.getProject()).getSelectedTextEditor();
         if (currentEditor != null) {
             VirtualFile currentFile = FileDocumentManager.getInstance().getFile(currentEditor.getDocument());
-            String fileName = currentFile.getName();
-            if (fileName.endsWith(".scala.html")) {
-                String targetName = fileName.replace(".scala.html", ".template.scala.html");
-                VirtualFile targetFile = e.getProject().getBaseDir().findChild("target").findChild("templates").findChild(targetName);
-
-                int lineInFile = currentEditor.getCaretModel().getPrimaryCaret().getLogicalPosition().line + 1;
-                Document targetDoc = FileDocumentManager.getInstance().getDocument(targetFile);
-                //Document currentDoc = FileDocumentManager.getInstance().getDocument(currentFile);
-                BreakpointManager breakPointManager = DebuggerManagerEx.getInstanceEx(e.getProject()).getBreakpointManager();
-                int lineInTarget = findLineInTarget(lineInFile, targetDoc);
-                Breakpoint bkp = activeBreakpoints.remove(makeKey(targetName, lineInTarget));
-                if (bkp != null) {
-                    breakPointManager.removeBreakpoint(bkp);
-                } else {
-                    Breakpoint addedBkp = breakPointManager.addLineBreakpoint(targetDoc, lineInTarget);
-                    activeBreakpoints.put(makeKey(targetName, lineInTarget), addedBkp);
-                    //breakPointManager.addLineBreakpoint(currentDoc, lineInFile);
-                }
-
-                System.out.print("work is done");
+            if (currentFile.getName().endsWith(".scala.html")) {
+                toggleBreakpointOnTemplate(e, currentEditor, currentFile);
             }
-
-
         }
     }
 
+    private void toggleBreakpointOnTemplate(AnActionEvent e, Editor currentEditor, VirtualFile currentFile) {
+        String fileName = currentFile.getName();
+        final String targetName = fileName.replace(".scala.html", ".template.scala.html");
+        final VirtualFile targetFile = e.getProject().getBaseDir().findChild("target").findChild("templates").findChild(targetName);
+        final Document targetDoc = FileDocumentManager.getInstance().getDocument(targetFile);
+        final int lineInFile = getCurrentLineInFile(currentEditor);
+        final int lineInTarget = findLineInTarget(lineInFile, targetDoc);
+        BreakpointManager breakPointManager = DebuggerManagerEx.getInstanceEx(e.getProject()).getBreakpointManager();
+        Breakpoint breakpointOnTarget = activeBreakpointsOnTarget.remove(makeKey(targetName, lineInTarget));
+
+        if (breakpointOnTarget != null) {
+            Breakpoint breakpointOnSource = activeBreakpointsOnSource.remove(makeKey(fileName, lineInFile));
+
+            breakPointManager.removeBreakpoint(breakpointOnTarget);
+            breakPointManager.removeBreakpoint(breakpointOnSource);
+        } else {
+            Document currentDoc = FileDocumentManager.getInstance().getDocument(currentFile);
+
+            Breakpoint addedBkpTarget = breakPointManager.addLineBreakpoint(targetDoc, lineInTarget);
+            Breakpoint addedBkpSource = breakPointManager.addLineBreakpoint(currentDoc, lineInFile);
+
+            activeBreakpointsOnSource.put(makeKey(fileName, lineInFile), addedBkpSource);
+            activeBreakpointsOnTarget.put(makeKey(targetName, lineInTarget), addedBkpTarget);
+        }
+    }
+
+    private int getCurrentLineInFile(Editor currentEditor) {
+        return currentEditor.getCaretModel().getPrimaryCaret().getLogicalPosition().line + 1;
+    }
+
     private int findLineInTarget(int lineInFile, Document targetDoc) {
-        return 7;
+        final String[] lines = targetDoc.getText().split("\n");
+        final String originalLineTag = "/*" + lineInFile + ".";
+        int lineCount = 0;
+        for (String line : lines) {
+            if (line.contains(originalLineTag)) {
+                return lineCount;
+            }
+            lineCount++;
+        }
+        return -1;
     }
 }
